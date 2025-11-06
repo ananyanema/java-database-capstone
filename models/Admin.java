fa950c2f3fc547dbd2d8176706a4a3dcf28ad6d2
ananyanema <ananyanema27@gmail.com>
2025-11-06 14:37:39 +0530

    Added Admin model with JPA and validation

---
+++ models/Admin.java
@@ -0,0 +1,43 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "username cannot be null")
+    private String username;
+
+    @NotNull(message = "password cannot be null")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // Default constructor
+    public Admin() {}
+
+    // Constructor with fields
+    public Admin(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
